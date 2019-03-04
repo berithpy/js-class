0e0061ded40f1188cd36d3caaea8350c0ca51982
Mateo <mateonavarro010@gmail.com>
2019-03-04 19:56:33 -0300

    calc just works LOL

--- cli/calc.js
+++ cli/calc.js
@@ -11,52 +11,62 @@ Necesitamos
 
 const readlineSync = require('readline-sync');
 
-function menu(){
-
-    console.log('este es el menu');
-}
-
-function suma(na,nb){
-    return na+nb
-}
-
-function multiplicación(na,nb) {
-    return na*nb
-}
-
-function resta (na,nb){
-    return na-nb
-}
-
-function division (na,nb){
-    return na/nb
-}
-
-
-menu();
-console.log("1. Suma"); 
-console.log("2. Resta");
-console.log("3. Multiplicacion");
-console.log("4. Division");
-const Operacion = Number(readlineSync.question("Qué operación desea realizar? Ver lista arriba"));
-
-
-switch (Operacion) {
+function menu() {
+  console.log("1. Suma");
+  console.log("2. Resta");
+  console.log("3. Multiplicacion");
+  console.log("4. Division");
+  const Operacion = Number(readlineSync.question("Qué operación desea realizar? Ver lista arriba "));
+  let resultado = 0;
+  switch (Operacion) {
     case 1:
-        rSuma=suma(a,b);
-        console.log(rSuma);
-        break;
+      sum_list = numberPromptMenu("Primer sumando: " ,"Segundo sumando: ")
+      resultado = suma(sum_list[0], sum_list[1]);
+      break;
+
     case 2:
-        rResta=resta(a,b);
-        console.log(rResta);
+      resta_list = numberPromptMenu("Minuendo: " ,"Sustraendo: ")
+      resultado = resta(resta_list[0], resta_list[1]);
+      break;
+
     case 3:
-        rMultiplicacion = multiplicacion(a,b);
-        console.log(rMultiplicacion);
+      multi_list = numberPromptMenu("Ingrese el Multiplicando: " ,"Ingrese el Multiplicador: ")
+      resultado = multiplicación(multi_list[0], multi_list[1]);
+      break;
+
     case 4:
-        rDivision=division(a,b);
-        console.log(rDivision);
-    
-    default:console.log("este valor no es valido")
-        break;
+      divi_list = numberPromptMenu("Ingrese el Dividendo: ","Ingrese el Divisor: ")
+      resultado = division(divi_list[0], divi_list[1]);
+      break;
+
+    default: 
+      console.log("este valor no es valido")
+      break;
+  }
+  console.log(`el resultado es ${resultado}`)
 }
-const
+function suma(na, nb) {
+  return na + nb
+}
+
+function multiplicación(na, nb) {
+  return na * nb
+}
+
+function resta(na, nb) {
+  return na - nb
+}
+
+function division(na, nb) {
+  return na / nb
+}
+
+function numberPromptMenu(firstNumberText,secondNumberText){
+  let a = Number(readlineSync.question(firstNumberText));
+  let b = Number(readlineSync.question(secondNumberText));
+  let listaTemporal=[a,b];
+  return listaTemporal;
+}
+while (true) {
+  menu();
+}
