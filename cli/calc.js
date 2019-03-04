a4ae7c3bdc6dcf689fc34c4604b2991bdb925466
Diego Diaz <berith.py@gmail.com>
2019-03-04 18:22:03 -0300

    Adds calc file.

---
+++ cli/calc.js
@@ -0,0 +1,10 @@
+/* 
+Calculadora 
+
+El programa te pregunta que operacion quieren usar y calcular
+
+Necesitamos 
+* Funcion para sumar,restar, multiplicar, dividr...
+* Interfaz
+* Devolver resultado
+*/
