f522c5da40ebb52efc6cd1f348893e2d86a94afa
Diego Diaz <berith.py@gmail.com>
2019-03-06 22:46:30 -0300

    Agrega concatenacion de array, arrayAlternadoConcat, arrayAlternado, intercambio, selectionSort.

--- cli/lists.js
+++ cli/lists.js
@@ -7,7 +7,9 @@ Listas
 * ------------------- que retorne los elementos en las posiciones impares
 * ------------------- que verifique si un string es un palindromo
 * ------------------- que suma todos los elementos del array con un for
-
+* ------------------- que concatene dos listas
+* ------------------- que combine dos listas alternando sus elementos
+                      que ordene elementos de una lista ver en wiki
 */
 
 let exampleArray = [1, 2, 'e', 9, 5];
@@ -77,21 +79,21 @@ function sumaArray(array) {
     for (let index = 0; index < array.length; index++) {
         suma = suma + array[index];
     }
-    return suma 
+    return suma
 }
 
-function sumaWhile(array){
-    let x=0;
-    let suma=0;
-    while (x<array.length) {
-        suma = suma+array[x];
+function sumaWhile(array) {
+    let x = 0;
+    let suma = 0;
+    while (x < array.length) {
+        suma = suma + array[x];
         x++;
     }
     return suma
 }
 
-function sumaRecursion(array, suma=0,contador=0){
-    if (contador>=array.length){
+function sumaRecursion(array, suma = 0, contador = 0) {
+    if (contador >= array.length) {
         return suma
     }
     suma += array[contador];
@@ -99,4 +101,88 @@ function sumaRecursion(array, suma=0,contador=0){
     return sumaRecursion(array, suma, contador);
 }
 
-console.log(sumaRecursion([1,2,3]))
+
+function concatArray(array1, array2) {
+    for (let index = 0; index < array2.length; index++) {
+        array1.push(array2[index]);
+    }
+    return array1
+}
+
+function arrayAlternadoConcat(arrayA, arrayB) {
+    let alterArray = [];
+    let largoDeArrayMayor = 0
+    if (arrayA.length >= arrayB.length) {
+        largoDeArrayMayor = arrayA.length
+        largoMenor = arrayB.length
+    }
+    else {
+        largoDeArrayMayor = arrayB.length
+        largoMenor = arrayA.length
+    }
+    for (let index = 0; index < largoMenor; index++) {
+        alterArray.push(arrayA[index]);
+        alterArray.push(arrayB[index]);
+    }
+    if (largoDeArrayMayor > arrayA.length) {
+        alterArray = concatArray(alterArray, arrayB.splice(largoMenor))
+    } else {
+        alterArray = concatArray(alterArray, arrayB.splice(largoMenor))
+    }
+    return alterArray
+}
+
+function arrayAlternado(arrayA, arrayB) {
+    let alterArray = [];
+    let largoDeArrayMayor = 0
+    if (arrayA.length >= arrayB.length) {
+        largoDeArrayMayor = arrayA.length
+    }
+    else {
+        largoDeArrayMayor = arrayB.length
+    }
+    for (let index = 0; index < largoDeArrayMayor; index++) {
+        const elementA = arrayA[index]
+        const elementB = arrayB[index]
+        if (elementA != undefined) {
+            alterArray.push(elementA);
+        }
+        if (elementB != undefined) {
+            alterArray.push(elementB);
+        }
+    }
+    return alterArray
+}
+
+
+// Selection sort https://es.wikipedia.org/wiki/Ordenamiento_por_selecci%C3%B3n
+
+function intercambiar(array, posicion1, posicion2) {
+    let valorTemporal = array[posicion1]
+    array[posicion1] = array [posicion2]
+    array[posicion2] = valorTemporal
+    return array
+}
+
+// 
+function selectionSort(array) {
+    var temp = 0;
+    for (let i = 0; i < array.length - 1; i++) {
+        min = i;
+        for (let j = i + 1; j < array.length; j++) {
+            if (array[j] < array[min]) {
+                min = j;
+            }
+        }
+        temp = array[min];
+        array[min] = array[i];
+        array[i] = temp;
+    }
+    return array;
+}
+
+var array1 = [8, 5, 3, 4];
+console.log(selectionSort(array1))
+
+
+
